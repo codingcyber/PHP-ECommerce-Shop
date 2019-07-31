90a4911361b93bbcc35889f470ac8a1f394f5181
codingcyber <vivek@codingcyber.com>
2019-07-31 15:12:38 +0530

    7-4 Displaying Cart Items in cart.php file'

--- cart.php
+++ cart.php
@@ -1,4 +1,8 @@
-<?php include('includes/header.php'); ?>
+<?php 
+session_start();
+require_once('includes/connect.php');
+include('includes/header.php'); 
+?>
 <!-- SHOP CONTENT -->
 <section id="content">
 	<div class="content-blog">
@@ -22,58 +26,52 @@
 				</tr>
 			</thead>
 			<tbody>
+			<?php 
+				if(isset($_SESSION['cart'])){
+					$cart = $_SESSION['cart'];
+					$total = 0;
+					foreach ($cart as $key => $value) {
+						// key is id and value is qunatity
+						$sql = "SELECT * FROM products WHERE id=?";
+					    $result = $db->prepare($sql);
+					    $result->execute(array($key));
+					    $product = $result->fetch(PDO::FETCH_ASSOC);
+			 ?>
 				<tr>
 					<td>
-						<a class="remove"><i class="fa fa-times"></i></a>
+						<a href="del-cart.php?id=<?php echo $key; ?>" class="remove"><i class="fa fa-times"></i></a>
 					</td>
 					<td>
-						<a href="#"><img src="images/shop/1.jpg" alt="" height="90" width="90"></a>					
+						<a href="product.php?id=<?php echo $product['id']; ?>"><img src="<?php echo $product['image']; ?>" alt="" height="90" width="90"></a>					
 					</td>
 					<td>
-						<a href="#">Product 1</a>					
+						<a href="#"><?php echo $product['title']; ?></a>					
 					</td>
 					<td>
-						<span class="amount">&#8377;69.99</span>					
+						<span class="amount">&#8377;<?php echo $product['price']; ?></span>					
 					</td>
 					<td>
-						<div class="quantity">1</div>
+						<div class="quantity"><?php echo $value['quantity']; ?></div>
 					</td>
 					<td>
-						<span class="amount">&#8377;69.99</span>					
+						<span class="amount">&#8377;<?php echo ($product['price']*$value['quantity']); ?></span>					
 					</td>
 				</tr>
-				<tr>
-					<td>
-						<a class="remove"><i class="fa fa-times"></i></a>
-					</td>
-					<td>
-						<a href="#"><img src="images/shop/2.jpg" alt="" height="90" width="90"></a>					
-					</td>
-					<td>
-						<a href="#">Product 2</a>					
-					</td>
-					<td>
-						<span class="amount">&#8377;119.99</span>					
-					</td>
-					<td>
-						<div class="quantity">1</div>
-					</td>
-					<td>
-						<span class="amount">&#8377;119.99</span>					
-					</td>
-				</tr>
+			<?php
+				$total = $total + ($product['price']*$value['quantity']);
+				} }else{ echo "<tr><td><h3>Add Products to Cart for Checkout.</h3></td></tr>"; } ?>
 				<tr>
 					<td colspan="6" class="actions">
 						<div class="col-md-6">
-							<div class="coupon">
+							<!-- <div class="coupon">
 								<label>Coupon:</label><br>
 								<input placeholder="Coupon code" type="text"> <button type="submit">Apply</button>
-							</div>
+							</div> -->
 						</div>
 						<div class="col-md-6">
 							<div class="cart-btn">
-								<button class="button btn-md" type="submit">Update Cart</button>
-								<button class="button btn-md" type="submit">Checkout</button>
+								<!-- <button class="button btn-md" type="submit">Update Cart</button> -->
+								<a href="checkout.php" class="button btn-md" >Checkout</a>
 							</div>
 						</div>
 					</td>
@@ -88,7 +86,7 @@
 					<tbody>
 						<tr>
 							<th>Cart Subtotal</th>
-							<td><span class="amount">&#8377;190.00</span></td>
+							<td><span class="amount">&#8377;<?php if(!empty($total)){ echo $total; }else{ echo "0"; } ?></span></td>
 						</tr>
 						<tr>
 							<th>Shipping and Handling</th>
@@ -98,7 +96,7 @@
 						</tr>
 						<tr>
 							<th>Order Total</th>
-							<td><strong><span class="amount">&#8377;190.00</span></strong> </td>
+							<td><strong><span class="amount">&#8377;<?php if(!empty($total)){ echo $total; }else{ echo "0"; } ?></span></strong> </td>
 						</tr>
 					</tbody>
 				</table>
