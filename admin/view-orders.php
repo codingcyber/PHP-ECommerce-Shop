5def1c478ff2cc0353300c4894f6a2dd19b6123b
codingcyber <vivek@codingcyber.com>
2019-11-29 19:20:42 +0530

    10-1 Displaying Orders in Backend

--- admin/view-orders.php
+++ admin/view-orders.php
@@ -1,5 +1,15 @@
-<?php include('includes/header.php'); ?>
-<?php include('includes/navigation.php'); ?>
+<?php 
+require_once('../includes/connect.php');
+include('includes/check-login.php');
+include('includes/header.php');
+include('includes/navigation.php'); 
+// Select query to get the order details from orders table
+// combine users table to get the username
+$sql = "SELECT o.id, o.created, o.amount, u.username FROM orders o JOIN users u ON o.uid=u.id";
+$result = $db->prepare($sql);
+$result->execute();
+$orders = $result->fetchAll(PDO::FETCH_ASSOC);
+?>
 <div id="page-wrapper" style="min-height: 345px;">
     <div class="row">
         <div class="col-lg-12">
@@ -20,35 +30,39 @@
                         <table class="table table-hover">
                             <thead>
                                 <tr>
-                                    <th>#</th>
-                                    <th>Title</th>
                                     <th>Order</th>
-                                    <th>Author</th>
+                                    <th>Customer Name</th>
                                     <th>Date</th>
+                                    <th>Total</th>
+                                    <th>Products</th>
+                                    <th>Status</th>
+                                    <th>Operations</th>
                                 </tr>
                             </thead>
                             <tbody>
+                                <?php 
+                                foreach ($orders as $order) { 
+                                    // get the number of order items from order_items table
+                                    $sql = "SELECT * FROM order_items WHERE orderid=?";
+                                    $result = $db->prepare($sql);
+                                    $result->execute(array($order['id']));
+                                    $ordercount = $result->rowCount();
+                                    // get the order status from order_status table
+                                    $sql = "SELECT * FROM order_status WHERE orderid=? ORDER BY id DESC LIMIT 1";
+                                    $result = $db->prepare($sql);
+                                    $result->execute(array($order['id']));
+                                    $status = $result->fetch(PDO::FETCH_ASSOC);
+                                    ?>
                                 <tr>
-                                    <td>1</td>
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
-                                    <td>@mdo</td>
+                                    <td><?php echo $order['id']; ?></td>
+                                    <td><?php echo $order['username']; ?></td>
+                                    <td><?php echo $order['created']; ?></td>
+                                    <td><?php echo $order['amount']; ?></td>
+                                    <td><?php echo $ordercount; ?></td>
+                                    <td><?php echo $status['status']; ?></td>
+                                    <td><a href="process-order.php?id=<?php echo $order['id']; ?>">Process Order</a></td>
                                 </tr>
-                                <tr>
-                                    <td>2</td>
-                                    <td>Jacob</td>
-                                    <td>Thornton</td>
-                                    <td>@fat</td>
-                                    <td>@mdo</td>
-                                </tr>
-                                <tr>
-                                    <td>3</td>
-                                    <td>Larry</td>
-                                    <td>the Bird</td>
-                                    <td>@twitter</td>
-                                    <td>@mdo</td>
-                                </tr>
+                                <?php } ?>
                             </tbody>
                         </table>
                     </div>
